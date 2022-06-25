5d67cc0417fdd745f018a39576857d82cba93db7
*GeoxTech* <*gibbs499@gmail.com*>
2022-06-25 04:31:54 +0100

    Update on login script

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,37 +1,45 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class Login {
 
-    //Import the Selenium WebDriver
-    private WebDriver driver;
+        //Import the Selenium WebDriver
+        private WebDriver driver;
 
-    @BeforeTest
-    public void start() throws InterruptedException {
-        //Locate chromedriver
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        @BeforeTest
+        public void start() throws InterruptedException {
+            //Locate chromedriver
+            System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
 
-        //1. Open Chrome browser
-        //Import Selenuim Web-driver
-        driver = new ChromeDriver();
+            //1. Open Chrome browser
+            //Import Selenuim Web-driver
+            driver = new ChromeDriver();
 
-        //2. Input your selenium Demo Page URL (https://primso-staging.lawpavilion.com/)
-        driver.get("https://primso-staging.lawpavilion.com/");
-        //Assertion @1
-        if (driver.getCurrentUrl().contains("https://primso-staging.lawpavilion.com/"))
-            //Pass
-            System.out.println("Correct Webpage");
-        else
-            //Fail
-            System.out.println("Wrong Webpage");
+            //2. Input your selenium Demo Page URL (https://primso-staging.lawpavilion.com/)
+            driver.get("https://primso-staging.lawpavilion.com/");
+            //Assertion @1
+            if (driver.getCurrentUrl().contains("https://primso-staging.lawpavilion.com/"))
+                //Pass
+                System.out.println("Correct Webpage");
+            else
+                //Fail
+                System.out.println("Wrong Webpage");
 
-        Thread.sleep(1000);
-        //3. Maximize the Chrome browser
-        driver.manage().window().maximize();
+            Thread.sleep(1000);
+            //3. Maximize the Chrome browser
+            driver.manage().window().maximize();
 
-        //4 Get the Page title for Primsol Page title
-        System.out.println(driver.getTitle());
-    }
+            //4 Get the Page title for Primsol Page title
+            System.out.println(driver.getTitle());
+        }
+
+        @AfterTest
+        public void closeBrowser () {
+            //Quit the browser
+            driver.quit();
+
+        }
 
 }
